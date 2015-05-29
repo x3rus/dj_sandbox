370ca72b8b0aeaf9a78e5961943e797db9b0975c
Thomas Boutry <thomas.boutry@x3rus.com>
2015-05-29 08:00:23 -0400

    Parametrisation de la configuration de l'interface admin pour l'application polls afin de pouvour éditer les questions et réponse simplement ... permettra la recherche et le trie par champs

--- sandbox/polls/admin.py
+++ sandbox/polls/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+# Permet l'ajout de Reponse lors de la creation de question
+# Stackedline et TabularInline dont 2 maniere d'afficher les reponses.
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # Ajout de la liste des choix possibles 
+    inlines = [ChoiceInline]
+
+    # amelioration des champs visible lors du listing
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # Ajout d'un champ de recherche
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
