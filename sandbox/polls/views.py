89f887558a615967dcc2de47bd1a817b8aa2d8a0
Thomas Boutry <thomas.boutry@x3rus.com>
2015-05-29 17:24:02 -0400

    version courte pour l'affichage de template

--- sandbox/polls/views.py
+++ sandbox/polls/views.py
@@ -6,16 +6,17 @@ from django.http import HttpResponse
 # Import le models pour pourvoir interoger la BD
 from .models import Question
 
-from django.template import RequestContext, loader
+# module pour la version longue
+##from django.template import RequestContext, loader
+
+# module version courte de l'affichage avec template
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
